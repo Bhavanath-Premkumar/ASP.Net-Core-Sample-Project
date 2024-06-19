6002a0441aa20a7c86ffd0953a1d934c7dc24a3f
Bhavanath-Premkumar <bhavanath.premkumar@techjays.com>
2024-06-19 16:29:06 +0530

    Intial Commit
    Uploaded the whole project

---
+++ Sample API Project/Sample API Project/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample_API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private List<string> users = new List<string>() { "Arun", "Vel", "Ram", "Prabu", "Vicky" };
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            if (users != null && users.Count > 0)
+                return Ok(users);
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            return id >= 0 && id < users.Count ? Ok(users[id]) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(string user)
+        {
+            if (!string.IsNullOrEmpty(user))
+                users.Add(user);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, string user)
+        {
+            if (id >= 0 && id < users.Count && !string.IsNullOrEmpty(user))
+                users[id] = user;
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+            if(id >=0 && id < users.Count) users.RemoveAt(id);
+
+            return NoContent();
+        }
+    }
+}
